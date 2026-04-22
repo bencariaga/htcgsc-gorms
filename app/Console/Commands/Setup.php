d863244c1bf4a62847f6b24bde3ced359d3a9f98
bencariaga <bencariaga13@gmail.com>
2026-04-23 05:55:02 +0800

    .

--- app/Console/Commands/Setup.php
+++ app/Console/Commands/Setup.php
@@ -14,14 +14,11 @@ class Setup extends BaseCommand
 
         $this->call('key:generate', ['--ansi' => true]);
 
-        $this->call('migrate', [
-            '--path' => 'database/migrations/special/nuke_database.php',
-            '--ansi' => true,
-            '--force' => true,
-        ]);
+        $this->components->task('Wiping database', function () {
+            $this->callSilent('db:wipe', ['--force' => true]);
+        });
 
         $this->call('migrate', [
-            '--drop-views' => true,
             '--path' => 'database/migrations/laravel',
             '--ansi' => true,
             '--force' => true,
@@ -35,7 +32,6 @@ class Setup extends BaseCommand
             'database/migrations/system/create_referrals_table.php',
             'database/migrations/system/create_appointments_table.php',
             'database/migrations/system/create_reports_table.php',
-            'database/migrations/special/add_auto_increment.php',
             'database/migrations/system/create_all_activities_view.php',
         ];
 
@@ -49,6 +45,7 @@ class Setup extends BaseCommand
 
         $this->call('db:seed', ['--ansi' => true, '--force' => true]);
         $this->call('storage:link', ['--ansi' => true]);
+
         $this->components->info('System setup has been completed successfully!');
 
         return Command::SUCCESS;
