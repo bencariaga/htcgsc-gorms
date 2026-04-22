b5270db8e493eb657866c577f5665881fecfd048
bencariaga <bencariaga13@gmail.com>
2026-04-22 23:24:11 +0800

    .

--- config/database.php
+++ config/database.php
@@ -56,18 +56,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'db_url' => env('DB_URL'),
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-            // 'charset' => env('DB_CHARSET', 'utf8'),
+            'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            // 'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'prefer',
         ],
     ],
 
