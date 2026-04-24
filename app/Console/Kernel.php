2b8c375f9e576e02a29f39c18f7811cafa73ac54
bencariaga <bencariaga13@gmail.com>
2026-04-24 10:10:18 +0800

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,13 +2,17 @@
 
 namespace App\Console;
 
-use Illuminate\{Console\Scheduling\Schedule, Foundation\Console\Kernel as ConsoleKernel};
+use Illuminate\{Console\Scheduling\Schedule, Foundation\Console\Kernel as ConsoleKernel, Support\Facades\Http};
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('internet:check')->everyMinute()->runInBackground();
+
+        $schedule->call(function () {
+            Http::get(config('app.url'));
+        })->everyFifteenMinutes();
     }
 
     protected function commands(): void
