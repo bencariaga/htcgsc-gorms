d68a68e3b78411e966d1feb2d88790667be6b394
bencariaga <bencariaga13@gmail.com>
2026-05-03 15:56:26 +0800

    .

--- app/Components/Molecules/Forms/ProfileActionBar.php
+++ app/Components/Molecules/Forms/ProfileActionBar.php
@@ -2,6 +2,7 @@
 
 namespace App\Components\Molecules\Forms;
 
+use App\Enums\NonDB\ProfileFormStyling;
 use Illuminate\{Support\Collection, View\Component};
 
 class ProfileActionBar extends Component
@@ -10,35 +11,7 @@ class ProfileActionBar extends Component
 
     public function __construct(public string $submitText = 'Save Changes', public string $resetText = 'Reset to Default', public string $passwordButtonText = 'Change Password', public bool $showPasswordButton = false)
     {
-        $this->buttons = collect([
-            $this->showPasswordButton ? [
-                'type' => 'button',
-                'action' => 'onclick="toggleModal(true)"',
-                'width' => 'w-[14rem]',
-                'colors' => 'text-blue-600 dark:text-blue-400 hover:bg-blue-100 dark:hover:bg-slate-700',
-                'icon' => 'fas fa-key',
-                'text' => $this->passwordButtonText,
-                'disabled' => null,
-            ] : null,
-            [
-                'type' => 'button',
-                'action' => '@click="resetForm()"',
-                'width' => 'w-[12.5rem]',
-                'colors' => 'text-orange-600 dark:text-orange-500 hover:bg-orange-100 dark:hover:bg-orange-900/40',
-                'icon' => 'fas fa-rotate-left group-hover:rotate-[-45deg]',
-                'text' => $this->resetText,
-                'disabled' => ':disabled="!anyDirty"',
-            ],
-            [
-                'type' => 'submit',
-                'action' => '',
-                'width' => 'w-[12rem]',
-                'colors' => 'text-emerald-600 dark:text-emerald-500 hover:bg-emerald-100 dark:hover:bg-emerald-900/90',
-                'icon' => 'fas fa-floppy-disk opacity-80',
-                'text' => $this->submitText,
-                'disabled' => ':disabled="!anyDirty"',
-            ],
-        ])->filter();
+        $this->buttons = collect(ProfileFormStyling::getActionButtons($this->submitText, $this->resetText, $this->passwordButtonText, $this->showPasswordButton));
     }
 
     public function render()
--- app/Enums/NonDB/ProfileFormStyling.php
+++ app/Enums/NonDB/ProfileFormStyling.php
@@ -54,4 +54,15 @@ enum ProfileFormStyling
 
         return compact('label', 'icon', 'required', 'colSpan', 'extra', 'type', 'class');
     }
+
+    public static function getActionButtons(string $submitText, string $resetText, string $passwordText, bool $showPassword): array
+    {
+        $buttons = [
+            $showPassword ? ['type' => 'button', 'action' => 'onclick="toggleModal(true)"', 'width' => 'w-[14rem]', 'colors' => 'text-blue-600 dark:text-blue-400 hover:bg-blue-100 dark:hover:bg-slate-700', 'icon' => 'fas fa-key', 'text' => $passwordText, 'disabled' => null] : null,
+            ['type' => 'button', 'action' => '@click="resetForm()"', 'width' => 'w-[12.5rem]', 'colors' => 'text-orange-600 dark:text-orange-500 hover:bg-orange-100 dark:hover:bg-orange-900/40', 'icon' => 'fas fa-rotate-left group-hover:rotate-[-45deg]', 'text' => $resetText, 'disabled' => ':disabled="!anyDirty"'],
+            ['type' => 'submit', 'action' => '', 'width' => 'w-[12rem]', 'colors' => 'text-emerald-600 dark:text-emerald-500 hover:bg-emerald-100 dark:hover:bg-emerald-900/90', 'icon' => 'fas fa-floppy-disk opacity-80', 'text' => $submitText, 'disabled' => ':disabled="!anyDirty"'],
+        ];
+
+        return collect($buttons)->filter()->values()->all();
+    }
 }
